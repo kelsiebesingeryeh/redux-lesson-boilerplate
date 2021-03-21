07b5e38f6703d55e838e38f272a1dbdd21d4beb9
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-03-21 16:21:56 -0600

    Connect add to do action to the store

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import AddTodoForm from './AddTodoForm';
+import React, { Component } from 'react'
+import AddTodoForm from '../containers/AddTodoForm'
+import ToDoList from '../containers/ToDoList'
 
 class App extends Component {
   render() {
@@ -7,9 +8,10 @@ class App extends Component {
       <div className="App">
         <h1>Another Todo Box</h1>
         <AddTodoForm />
+        <ToDoList />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
